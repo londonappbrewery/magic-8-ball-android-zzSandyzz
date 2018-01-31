bdb2dda3681a598268c5ef7792b7aa9c7d6fb2c7
zzSandyzz <tendersymphony@gmail.com>
2018-02-01 00:28:33 +0300

    Project complete!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,50 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import java.util.Random;
+
+/*Set a Listener on myButton using the dot (.) notation.
+ Remember to use autocomplete to help you insert the code.
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+
+        Button  myButton =(Button) findViewById(R.id.askButton);
+        final int[] ballArray ={R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                /*Use setImageResource(), the ballArray,
+                and the number to give the ballDisplay a random 8 ball image to display.*/
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(5);
+
+                ballDisplay.setImageResource(ballArray[number]);
+
+            }
+        });
+
+
+
+
+
     }
 }
